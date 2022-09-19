3ef8635dc5516e50439e11e62625eef08f871e71
kushaldesai22 <20it024@charusat.edu.in>
2022-09-19 13:59:28 +0530

    done

---
+++ hello.js
@@ -0,0 +1,7 @@
+//console.log("hello world")
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('<h1>Hello World!</h1>');
+}).listen(3001);
