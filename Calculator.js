ca049e3520e920dd32a3574ba808151d2b6e3d5d
d21it178 <90238639+d21it178@users.noreply.github.com>
2022-09-20 13:11:45 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,32 @@
+//Create local NodeJS Custom Module “Calculator” and import it and use the module in current project.
+
+exports.add = function (x, y) { 
+    return x + y; 
+    
+}; 
+    
+exports.sub = function (x, y) { 
+    return x - y; 
+}; 
+    
+exports.mult = function (x, y) { 
+    return x * y; 
+}; 
+    
+exports.div = function (x, y) { 
+    return x / y; 
+};
+
+var calculator = require('./calcModule'); 
+var x = 50, y = 10; 
+console.log("Addition of 50 and 10 is "
+                   + calculator.add(x, y)); 
+    
+console.log("Subtraction of 50 and 10 is "
+                   + calculator.sub(x, y)); 
+    
+console.log("Multiplication of 50 and 10 is "
+                   + calculator.mult(x, y)); 
+    
+console.log("Division of 50 and 10 is " 
+                   + calculator.div(x, y)); 
