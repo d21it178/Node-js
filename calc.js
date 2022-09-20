c283fb634b4604a60628f46e15f55f94c683d593
d21it178 <90238639+d21it178@users.noreply.github.com>
2022-09-20 13:32:38 +0530

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,24 +1,16 @@
-var http = require('http');
+var calculator = require('./calcModule');
 
-http.createServer(function (req, res) {
-    
+var x = 50, y = 10;
 
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var calculator = require('./calcModule'); 
-    
-    var x = 50, y = 10; 
 
-    
-    res.write(" Addition of 50 and 10 is "
-                   + calculator.add(x, y)); 
-    
-                   res.write("\n Subtraction of 50 and 10 is "
-                   + calculator.sub(x, y)); 
-    
-                   res.write("\n Multiplication of 50 and 10 is "
-                   + calculator.mult(x, y)); 
-    
-                   res.end("\n Division of 50 and 10 is " 
-                   + calculator.div(x, y)); 
-                   res.send();
-                }).listen(3002);
+console.log("Addition of 50 and 10 is "
+                   + calculator.add(x, y));
+
+console.log("Subtraction of 50 and 10 is "
+                   + calculator.sub(x, y));
+
+console.log("Multiplication of 50 and 10 is "
+                   + calculator.mult(x, y));
+
+console.log("Division of 50 and 10 is "
+                   + calculator.div(x, y));
